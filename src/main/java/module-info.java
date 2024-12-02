355200ab5506cbb8ec17f516214791b88318909a
UnpolishedNoob <banikpritom321@gmail.com>
2024-12-02 13:10:45 +0600

    edited dashboard layout adjusted profile picture

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,7 @@ module com.example.i_connect {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+    //requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
     opens com.example.i_connect to javafx.fxml;
